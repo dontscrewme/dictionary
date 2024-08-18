b3037df9633e7f9bbcbe7d086850d34222eb003d
dontscrewme <57172435+dontscrewme@users.noreply.github.com>
2024-08-18 20:33:56 +0800

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,40 +1,53 @@
 #include "dictionary.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
-/** Minimal allocated number of entries in a dictionary */
-#define DICTMINSZ 128
+static int default_error_callback(const char *format, ...) {
+  int ret;
+  va_list argptr;
+  va_start(argptr, format);
+  ret = vfprintf(stderr, format, argptr);
+  va_end(argptr);
+  return ret;
+}
+
+int (*error_callback)(const char *, ...) = default_error_callback;
+
+void dictionary_set_error_callback(int (*errback)(const char *, ...)) {
+  if (errback) {
+    error_callback = errback;
+  }
+}
 
 static unsigned dictionary_hash(const char *key) {
-  size_t len;
-  unsigned hash;
-  size_t i;
 
-  if (!key)
-    return 0;
+	unsigned int hash = 0;
+	unsigned int len = strlen(key);
+	for (unsigned int i = 0; i < len; i++) {
+		hash += (unsigned)key[i];
+		hash += (hash << 10);
+		hash ^= (hash >> 6);
+	}
 
-  len = strlen(key);
-  for (hash = 0, i = 0; i < len; i++) {
-    hash += (unsigned)key[i];
-    hash += (hash << 10);
-    hash ^= (hash >> 6);
-  }
-  hash += (hash << 3);
-  hash ^= (hash >> 11);
-  hash += (hash << 15);
-  return hash;
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+
+	return hash;
 }
 
 static int dictionary_grow(struct dictionary *d) {
   struct bucket **new_table = calloc(d->size * 2, sizeof(struct bucket *));
-  if (new_table == NULL) {
+  if (!new_table) {
+    error_callback("%s: calloc() failed\n", __func__);
     return -1;
   }
 
   for (unsigned int i = 0; i < d->size; i++) {
     struct bucket *current = d->table[i];
-    while (current != NULL) {
-      unsigned int new_index = dictionary_hash(current->key) % (d->size * 2);
+    while (current) {
+			unsigned int new_index = dictionary_hash(current->key) % (d->size * 2);
       struct bucket *tmp = current->next;
       current->next = new_table[new_index];
       new_table[new_index] = current;
@@ -45,12 +58,16 @@ static int dictionary_grow(struct dictionary *d) {
   free(d->table);
   d->size *= 2;
   d->table = new_table;
+	
   return 0;
 }
 
+/** Minimal allocated number of entries in a dictionary */
+#define DICTMINSZ 128
 struct dictionary *dictionary_new(size_t size) {
   struct dictionary *d = malloc(sizeof(struct dictionary));
-  if (d == NULL) {
+  if (!d) {
+    error_callback("%s: malloc() failed\n", __func__);
     return NULL;
   }
 
@@ -60,25 +77,27 @@ struct dictionary *dictionary_new(size_t size) {
   }
 
   d->table = calloc(size, sizeof(struct bucket *));
-  if (d->table == NULL) {
+  if (!d->table) {
+    error_callback("%s: calloc() failed\n", __func__);
     free(d);
     return NULL;
   }
 
   d->size = size;
-  d->n = 0;
+  d->numOfElements = 0;
 
   return d;
 }
 
 void dictionary_del(struct dictionary *d) {
-  if (d == NULL) {
+  if (!d) {
+    error_callback("%s: invalid input\n", __func__);
     return;
   }
 
   for (unsigned i = 0; i < d->size; i++) {
     struct bucket *curr = d->table[i];
-    while (curr != NULL) {
+    while (curr) {
       struct bucket *prev = curr;
       curr = curr->next;
       free(prev->key);
@@ -93,13 +112,14 @@ void dictionary_del(struct dictionary *d) {
 
 const char *dictionary_get(const struct dictionary *d, const char *key,
                            const char *def) {
-  if (d == NULL || key == NULL) {
+  if (!d || !key) {
+    error_callback("%s: invalid input\n", __func__);
     return def;
   }
 
-  unsigned int index = dictionary_hash(key) % d->size;
+	unsigned int index = dictionary_hash(key) % d->size;
   struct bucket *curr = d->table[index];
-  while (curr != NULL) {
+  while (curr) {
     if (strcmp(curr->key, key) == 0) {
       return curr->value;
     }
@@ -110,16 +130,18 @@ const char *dictionary_get(const struct dictionary *d, const char *key,
 }
 
 int dictionary_set(struct dictionary *d, const char *key, const char *val) {
-  if (d == NULL || key == NULL || val == NULL) {
+  if (!d || !key || !val) {
+    error_callback("%s: invalid input\n", __func__);
     return -1;
   }
 
-  unsigned int index = dictionary_hash(key) % d->size;
+	unsigned int index = dictionary_hash(key) % d->size;
   struct bucket *curr = d->table[index];
-  while (curr != NULL) {
+  while (curr) {
     if (strcmp(curr->key, key) == 0) {
       char *new_val = strdup(val);
-      if (new_val == NULL) {
+      if (!new_val) {
+        error_callback("%s: strdup() failed\n", __func__);
         return -1;
       }
       free(curr->value);
@@ -129,48 +151,54 @@ int dictionary_set(struct dictionary *d, const char *key, const char *val) {
     curr = curr->next;
   }
 
-  if (d->n >= d->size * 0.7) {
+  if (d->numOfElements >= d->size * 0.7) {
     if (dictionary_grow(d) != 0) {
+      error_callback("%s: dictionary_grow() failed\n", __func__);
       return -1;
     }
   }
 
   struct bucket *new_bucket = malloc(sizeof(struct bucket));
-  if (new_bucket == NULL) {
+  if (!new_bucket) {
+    error_callback("%s: malloc() failed\n", __func__);
     return -1;
   }
 
   new_bucket->key = strdup(key);
-  if (new_bucket->key == NULL) {
+  if (!new_bucket->key) {
+    error_callback("%s: strdup() failed\n", __func__);
     free(new_bucket);
     return -1;
   }
   new_bucket->value = strdup(val);
-  if (new_bucket->value == NULL) {
+  if (!new_bucket->value) {
+    error_callback("%s: strdup() failed\n", __func__);
     free(new_bucket->key);
     free(new_bucket);
     return -1;
   }
+
   new_bucket->next = d->table[index];
   d->table[index] = new_bucket;
-  d->n++;
+  d->numOfElements++;
 
   return 0;
 }
 
 void dictionary_unset(struct dictionary *d, const char *key) {
-  if (key == NULL || d == NULL) {
+  if (!key || !d) {
+    error_callback("%s: invalid input\n", __func__);
     return;
   }
 
-  unsigned int index = dictionary_hash(key) % d->size;
+	unsigned int index = dictionary_hash(key) % d->size;
 
   struct bucket *curr = d->table[index];
   struct bucket *prev = NULL;
 
-  while (curr != NULL) {
+  while (curr) {
     if (strcmp(curr->key, key) == 0) {
-      if (prev == NULL) {
+      if (!prev) {
         d->table[index] = curr->next;
       } else {
         prev->next = curr->next;
@@ -178,7 +206,7 @@ void dictionary_unset(struct dictionary *d, const char *key) {
       free(curr->key);
       free(curr->value);
       free(curr);
-      d->n--;
+      d->numOfElements--;
       return;
     }
     prev = curr;
@@ -187,17 +215,19 @@ void dictionary_unset(struct dictionary *d, const char *key) {
 }
 
 void dictionary_dump(const struct dictionary *d, FILE *out) {
-
-  if (d == NULL || out == NULL)
+  if (!d || !out) {
+    error_callback("%s: invalid input\n", __func__);
     return;
-  if (d->n < 1) {
-    fprintf(out, "empty dictionary\n");
+  }
+
+  if (d->numOfElements < 1) {
+    error_callback("%s: empty dictionary\n", __func__);
     return;
   }
 
   for (unsigned int i = 0; i < d->size; i++) {
     struct bucket *curr = d->table[i];
-    if (curr != NULL) {
+    if (curr) {
       fprintf(out, "%20s\t[%s]\n", d->table[i]->key,
               d->table[i]->value ? d->table[i]->value : "UNDEF");
       curr = curr->next;
