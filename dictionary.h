259f2f5b61b8ae02c45a09884b09be25bd182eb5
dontscrewme <57172435+dontscrewme@users.noreply.github.com>
2025-06-19 17:58:18 +0800

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -1,6 +1,7 @@
 #ifndef _DICTIONARY_H_
 #define _DICTIONARY_H_
 
+extern int (*error_callback)(const char *, ...);
 void dictionary_set_error_callback(int (*errback)(const char *, ...));
 
 #include <stdio.h>
@@ -17,11 +18,12 @@ struct dictionary {
 	struct bucket **table;
 };
 
+unsigned dictionary_hash(const char *key);
 struct dictionary *dictionary_new(size_t size);
-void dictionary_del(struct dictionary *vd);
+void dictionary_del(struct dictionary *d);
 const char *dictionary_get(const struct dictionary *d, const char *key,
 													 const char *def);
-int dictionary_set(struct dictionary *d, const char *key, const char *val);
+int dictionary_set(struct dictionary *vd, const char *key, const char *val);
 void dictionary_unset(struct dictionary *d, const char *key);
 void dictionary_dump(const struct dictionary *d, FILE *out);
 
